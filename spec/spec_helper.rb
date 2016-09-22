be11690909965242ce04e742c97712d84c06da56
Nic Cavigliano <pair+ncavig@realscout.com>
2016-09-22 11:18:00 -0700

    Moves codeclimate start line to the top of spec_helper.rb to see if test coverage report works

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require 'contactually'
 require 'webmock/rspec'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
