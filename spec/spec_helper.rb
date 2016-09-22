a45eccada3a483202925457d2da8742b9dfca9d3
Nic Cavigliano <pair+ncavig@realscout.com>
2016-09-22 10:47:01 -0700

    Adds codeclimate support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'contactually'
 require 'webmock/rspec'
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
