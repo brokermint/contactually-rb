96d0020025edd291e580f12de6bab0815a0fbdea
Nic Cavigliano <pair+ncavig@realscout.com>
2016-09-22 11:24:18 -0700

    Whitelist codeclimate after test suite is complete

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
-
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'contactually'
 require 'webmock/rspec'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    WebMock.disable_net_connect!(:allow => 'codeclimate.com')
+  end
+end
