94d10e0d459b6ec2dbbc7dca9425668ae8ee9393
Mykhailo Odyniuk <m.odyniuk@logicsolutions.com.ua>
2019-01-29 17:36:31 +0200

    Add workaround for broken Contacts API  url

--- lib/contactually/contacts.rb
+++ lib/contactually/contacts.rb
@@ -10,5 +10,26 @@ module Contactually
     def model
       Models::Contact
     end
+
+    def list_in_batches(params)
+      collection = Response.new(interface.get(url, params), self).data
+      return unless collection.total > 0
+
+      while collection.items.any?
+        yield collection
+
+        break unless collection.has_next_page?
+
+
+        # The problem: Contactually API return 404 status code when we get next batch with `next_page` URL
+        #  from the response.
+        #
+        # HACK: API endpoint `/v2/contacts/search` doesn't work correct.
+        url = collection.next_page.gsub('/search', '')
+        _old_page = params.delete(:page)
+
+        collection = Response.new(interface.get(url, params), self).data
+      end
+    end
   end
 end
